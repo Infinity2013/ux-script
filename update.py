28ff5a2a0bc0c864208b69179a3cee3175e5c40a
Wu Xiaoliang <xiaoliangx.wu@intel.com>
2015-05-06 11:14:47 +0800

    update.py: sync with remote if new commit exist

---
+++ update.py
@@ -0,0 +1,39 @@
+import subprocess
+import os
+import sys
+import re
+from clog import clog
+
+clog = clog()
+clog.setLevel("v|e")
+def formatcheck(string):
+    if not re.findall("^\S+$", string.strip()):
+        return False
+    else:
+        return True
+
+def versioncheck():
+    clog.v("Checking updates!")
+    os.system("git reset HEAD --hard")
+    os.system("git remote update")
+    p = subprocess.Popen("git rev-parse HEAD", shell = True, stdout = subprocess.PIPE)
+    local_sha = p.stdout.readline().strip()
+    if not formatcheck(local_sha):
+        clog.e("Couldn't get local head!")
+        return
+    p = subprocess.Popen("git rev-parse @{u}", shell = True, stdout = subprocess.PIPE)
+    remote_sha = p.stdout.readline().strip()
+    if not formatcheck(remote_sha):
+        clog.e("Couldn't get remote head!")
+        return
+    if local_sha != remote_sha:
+        clog.v("Syncing updates!")
+        os.chdir(sys.path[0])
+        p = subprocess.Popen("git pull", shell = True, stdout = subprocess.PIPE)
+        if p.returncode != 0:
+            clog.e("Sync failed!")
+        else:
+            clog.v("Sync done!")
+            clog.v("Please run script again!")
+    else:
+        clog.v("Already update-latest!")
